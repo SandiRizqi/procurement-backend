3662dcd36ef687d2c79f7b26a778aef1dccfa2e0
unknown <User@DESKTOP-DO052JA.tap.corp>
2026-02-11 14:05:34 +0700

    add delete response

--- apps/persons/views.py
+++ apps/persons/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from .models import Person, PersonDocument
 from .serializers import PersonSerializer, PersonDocumentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
@@ -6,13 +7,22 @@ from django_filters.rest_framework import DjangoFilterBackend
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    permission_classes = [permissions.IsAuthenticated]  # bisa diganti custom permission
+    permission_classes = [permissions.IsAuthenticated, permissions.DjangoModelPermissions]  # bisa diganti custom permission
 
     filter_backends = [DjangoFilterBackend]
     filterset_fields = {
         'vendor__name': ['exact', 'icontains'],  # related field lookup
         'role': ['exact', 'icontains'],
     }
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        name = instance.full_name
+        self.perform_destroy(instance)
+        return Response(
+            {"message": f"Person '{name}' berhasil dihapus."},
+            status=status.HTTP_200_OK
+        )
 
 
 class PersonDocumentViewSet(viewsets.ModelViewSet):
